8f039d220fc7f1c5f17ccfb1559d6d3ba6ebc93b
mohamedfahil <mfahil26@gmail.com>
2024-05-21 00:23:37 +0530

    Create obj.js

---
+++ obj.js
@@ -0,0 +1,29 @@
+let obj1={name:"Person 1",
+          age:5
+}
+
+let obj2={age:5,
+          name:"Person 1"
+
+}
+for(key in obj1)
+    {
+        
+       let x =key
+       let y =obj1[key]
+        
+
+        let z=key
+        let m=obj2[key]
+        if(x,y==z,m)
+            {
+                console.log("true")
+            }else
+            {
+                console.log("false")
+            }
+        
+        
+        
+        
+    }
